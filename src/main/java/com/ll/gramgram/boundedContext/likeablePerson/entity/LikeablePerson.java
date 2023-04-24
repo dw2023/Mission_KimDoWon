6b93e6d29a4f62f1e51d6fda6eaeb9932d77d443
dw2023 <dwkim126@gmail.com>
2023-04-25 06:11:42 +0900

    refactor: 케이스 6

--- src/main/java/com/ll/gramgram/boundedContext/likeablePerson/entity/LikeablePerson.java
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/entity/LikeablePerson.java
@@ -24,6 +24,7 @@ public class LikeablePerson extends BaseEntity {
     private InstaMember toInstaMember; // 호감을 받은 사람(인스타 멤버)
     private String toInstaMemberUsername; // 혹시 몰라서 기록
 
+    @Setter
     private int attractiveTypeCode; // 매력포인트(1=외모, 2=성격, 3=능력)
 
     public String getAttractiveTypeDisplayName() {
@@ -32,9 +33,5 @@ public class LikeablePerson extends BaseEntity {
             case 2 -> "성격";
             default -> "능력";
         };
-    }
-
-    public void updateAttractiveType (int attractiveTypeCode) {
-        this.attractiveTypeCode = attractiveTypeCode;
     }
 }
